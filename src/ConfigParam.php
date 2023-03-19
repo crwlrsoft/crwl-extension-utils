57ab1819a7ef0a68d3b4dd38c1720f58da21a912
otsch <otsch@users.noreply.github.com>
2023-03-19 02:27:32 +0100

    Add (default) value to ConfigParam

--- src/ConfigParam.php
+++ src/ConfigParam.php
@@ -7,22 +7,28 @@ final class ConfigParam
     public function __construct(
         public readonly ConfigParamTypes $type,
         public readonly string $name,
+        public readonly mixed $value,
     ) {
     }
 
     public static function bool(string $paramName): self
     {
-        return new self(ConfigParamTypes::Bool, $paramName);
+        return new self(ConfigParamTypes::Bool, $paramName, false);
     }
 
     public static function int(string $paramName): self
     {
-        return new self(ConfigParamTypes::Int, $paramName);
+        return new self(ConfigParamTypes::Int, $paramName, 0);
     }
 
     public static function string(string $paramName): self
     {
-        return new self(ConfigParamTypes::String, $paramName);
+        return new self(ConfigParamTypes::String, $paramName, '');
+    }
+
+    public function default(mixed $defaultValue): self
+    {
+        return new self($this->type, $this->name, $defaultValue);
     }
 
     public function toArray(): array
