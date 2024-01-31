740d31ca02111c58f049f3c953749753cdf08944
otsch <otsch@users.noreply.github.com>
2024-01-31 13:01:39 +0100

    Throw exception when stepId does not contain dot
    To separate group and step name.

---
+++ src/Exceptions/InvalidStepBuilderException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Crwlr\CrwlExtensionUtils\Exceptions;
+
+use Exception;
+
+class InvalidStepBuilderException extends Exception
+{
+}
---
+++ tests/StepBuilderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests;
+
+use Crwlr\Crawler\Steps\Step;
+use Crwlr\Crawler\Steps\StepInterface;
+use Crwlr\CrwlExtensionUtils\Exceptions\InvalidStepBuilderException;
+use Crwlr\CrwlExtensionUtils\StepBuilder;
+use Generator;
+
+it('throws an exception when the stepId() does not contain a "."', function () {
+    new class () extends StepBuilder
+    {
+        public function stepId(): string
+        {
+            return 'groupName-stepName';
+        }
+
+        public function label(): string
+        {
+            return 'Step Label';
+        }
+
+        public function configToStep(array $stepConfig): StepInterface
+        {
+            return new class () extends Step
+            {
+                protected function invoke(mixed $input): Generator
+                {
+                    yield 'yolo';
+                }
+            };
+        }
+    };
+})->throws(InvalidStepBuilderException::class);
